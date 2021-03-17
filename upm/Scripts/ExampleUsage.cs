2b046c61f2f5a4e8e8530a85c11e24c64eb4e13a
Scott Doxey <neogeek0101@gmail.com>
2021-03-17 17:37:31 -0400

    Added example package.

---
+++ upm/Scripts/ExampleUsage.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace ExamplePackage
+{
+
+    public class ExampleUsage : MonoBehaviour
+    {
+
+        private void Start()
+        {
+
+#if ENABLE_INPUT_SYSTEM
+            Debug.Log("New Input System is enabled.");
+#else
+            Debug.Log("The legacy Input System is enabled.");
+#endif
+
+        }
+
+    }
+
+}
